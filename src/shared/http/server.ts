a537a1f9596e829f2c7d8a7a6d26e64cee3eed36
Jonathan Silva <84137692+JonathSilva@users.noreply.github.com>
2022-01-18 18:24:53 -0300

    upload api

---
+++ src/shared/http/server.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server Started on port 3333!`);
+});
